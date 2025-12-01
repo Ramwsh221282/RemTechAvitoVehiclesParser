e8051fcfb26452c6bfedb9ab72a525d9ae1714ab
Ramwsh221282 <jimkrauz@gmail.com>
2025-12-01 19:20:31 +0700

    added pagination scraper for avito

---
+++ RemTechAvitoVehiclesParser/Tests/PuppeteerTests/BrowserInstance.cs
@@ -0,0 +1,60 @@
+﻿using PuppeteerExtraSharp;
+using PuppeteerExtraSharp.Plugins.BlockResources;
+using PuppeteerExtraSharp.Plugins.ExtraStealth;
+using PuppeteerSharp;
+
+namespace Tests.PuppeteerTests;
+
+public sealed class BrowserInstance
+{
+    private IBrowser _browser = null!;
+    private IPage _page = null!;
+    private bool _instantiated;
+
+    public BrowserInstance()
+    {
+    }
+
+    public async Task Invoke(Func<IBrowser, Task> invoke)
+    {
+        await invoke(_browser);
+    }
+
+    public async Task Invoke(Func<IPage, Task> invoke)
+    {
+        await invoke(_page);
+    }
+
+    public async Task<U> Invoke<U>(Func<IPage, Task<U>> invoke)
+    {
+        return await invoke(_page);
+    }
+    
+    public async Task<U> Invoke<U>(Func<IBrowser, Task<U>> invoke)
+    {
+        return await invoke(_browser);
+    }
+
+    public async Task Destroy()
+    {
+        await _page.DisposeAsync();
+        await _browser.CloseAsync();
+    }
+    
+    public async Task Instantiate(bool headless = true)
+    {
+        if (_instantiated) return;
+        
+        LaunchOptions options = new LaunchOptions
+        {
+            Headless = headless
+        };
+        
+        PuppeteerExtra extra = new PuppeteerExtra();
+        extra.Use(new BlockResourcesPlugin()).Use(new StealthPlugin());
+        _browser = await extra.LaunchAsync(options);
+        IPage[]? pages = await _browser.PagesAsync();
+        _page = pages.First();
+        _instantiated = true;
+    }
+}
---
+++ RemTechAvitoVehiclesParser/Tests/PuppeteerTests/PuppeteerTestFixture.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Serilog;
+
+namespace Tests.PuppeteerTests;
+
+public sealed class PuppeteerTestFixture : IAsyncLifetime
+{
+    private readonly IServiceProvider _sp;
+
+    public IServiceProvider Services => _sp;
+    
+    public PuppeteerTestFixture()
+    {
+        IServiceCollection services = new ServiceCollection();
+        ILogger logger = new LoggerConfiguration().WriteTo.Console().CreateLogger();
+        services.AddSingleton(logger);
+        services.AddSingleton<BrowserDownloader>();
+        services.AddSingleton<BrowserInstance>();
+        _sp = services.BuildServiceProvider();
+    }
+    
+    public async Task InitializeAsync()
+    {
+        BrowserDownloader downloader = _sp.GetRequiredService<BrowserDownloader>();
+        await downloader.DownloadBrowser();
+        BrowserInstance instance = _sp.GetRequiredService<BrowserInstance>();
+        await instance.Instantiate(headless: false);
+    }
+
+    public async Task DisposeAsync()
+    {
+        BrowserInstance instance = _sp.GetRequiredService<BrowserInstance>();
+        await instance.Destroy();
+    }
+}
