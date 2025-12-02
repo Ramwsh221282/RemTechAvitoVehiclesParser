cdd5d7e0ade1d090c8542c1121e7565e2e414be2
Ramwsh221282 <jimkrauz@gmail.com>
2025-12-02 15:55:25 +0700

    added routing key (Type constant) for register parser on startup publishing

--- RemTechAvitoVehiclesParser/RemTechAvitoVehiclesParser/OnStartup/RegisterParser/RegisterParserOnStartup.cs
+++ RemTechAvitoVehiclesParser/RemTechAvitoVehiclesParser/OnStartup/RegisterParser/RegisterParserOnStartup.cs
@@ -6,10 +6,11 @@ namespace RemTechAvitoVehiclesParser.OnStartup.RegisterParser;
 public sealed class RegisterParserOnStartup(NpgSqlParserTicketsStorage storage) : IRegisterParserOnStartup
 {
     private readonly NpgSqlParserTicketsStorage _storage = storage;
+    private const string Type = "register.parser";
 
     public async Task Invoke(string domain, string type)
     {
-        ParserTicket ticket = new(Guid.NewGuid(), type, DateTime.UtcNow, null);
+        ParserTicket ticket = new(Guid.NewGuid(), Type, DateTime.UtcNow, null);
         ticket.AddPayloadElement("domain", domain);
         ticket.AddPayloadElement("type", type);
         await _storage.Add(ticket);
