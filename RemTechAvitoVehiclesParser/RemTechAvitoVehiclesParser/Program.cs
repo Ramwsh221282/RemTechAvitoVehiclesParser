71cf6b4cc80e12828fd554b3be9d07621fa8927f
Ramwsh221282 <jimkrauz@gmail.com>
2025-12-19 19:28:47 +0700

    updated parserSDK to 1.0.29

--- RemTechAvitoVehiclesParser/RemTechAvitoVehiclesParser/Program.cs
+++ RemTechAvitoVehiclesParser/RemTechAvitoVehiclesParser/Program.cs
@@ -1,37 +1,13 @@
-using AvitoFirewallBypass;
-using Microsoft.Extensions.Options;
-using ParsingSDK;
-using ParsingSDK.TextProcessing;
 using RemTech.SharedKernel.Infrastructure;
 using RemTechAvitoVehiclesParser;
-using RemTechAvitoVehiclesParser.ParserWorkStages;
-using RemTechAvitoVehiclesParser.Parsing;
-using RemTechAvitoVehiclesParser.SharedDependencies;
+using RemTechAvitoVehiclesParser.Parsing.BackgroundTasks;
 
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
-
-builder.Services.AddDbUpgrader();
-builder.Services.RegisterTextTransformerBuilder();
-builder.Services.RegisterParserWorkStagesContext();
-builder.Services.RegisterParserDependencies(conf =>
-{
-    IOptions<ScrapingBrowserOptions> options = Options.Create(new ScrapingBrowserOptions()
-    {
-        Headless = false,
-        BrowserPath = "C:\\Users\\ramwsh\\Desktop\\avito_vehicles_parser\\RemTechAvitoVehiclesParser\\RemTechAvitoVehiclesParser\\Tests\\bin\\Debug\\net10.0\\Chromium\\Win64-1559811\\chrome-win\\chrome.exe"
-    });
-    conf.AddSingleton(options);
-});
-builder.Services.RegisterAvitoParsing();
-builder.Services.RegisterAvitoFirewallBypass();
-builder.Services.RegisterSharedInfrastructure();
-builder.Services.RegisterParserSubscription();
-builder.Services.AddQuartzServices();
-
+builder.Services.RegisterDependenciesForParsing();
+builder.Services.RegisterAvitoStartQueue();
+builder.Services.RegisterInfrastructureDependencies();
 WebApplication app = builder.Build();
-
 app.Services.ApplyDatabaseMigrations();
-
 app.Run();
 
 namespace RemTechAvitoVehiclesParser
